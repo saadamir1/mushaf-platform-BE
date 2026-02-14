566e8ea7d339e25995b9ffa3ab67d7c4692af8bb
Saad Amir <Saadamir070@gmail.com>
2026-02-15 04:41:04 +0500

    fix(Quran): fix Quran module imports

--- src/quran/quran.module.ts
+++ src/quran/quran.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Surah } from './entities/surah.entity';
+import { Verse } from './entities/verse.entity';
+import { Juz } from './entities/juz.entity';
+import { QuranPage } from './entities/page.entity';
 import { SurahsModule } from './surahs/surahs.module';
 import { VersesModule } from './verses/verses.module';
 import { PagesModule } from './pages/pages.module';
@@ -6,6 +11,14 @@ import { JuzModule } from './juz/juz.module';
 import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [SurahsModule, VersesModule, PagesModule, JuzModule, SearchModule]
+  imports: [
+    TypeOrmModule.forFeature([Surah, Verse, Juz, QuranPage]),
+    SurahsModule,
+    VersesModule,
+    PagesModule,
+    JuzModule,
+    SearchModule,
+  ],
+  exports: [TypeOrmModule],
 })
 export class QuranModule {}
