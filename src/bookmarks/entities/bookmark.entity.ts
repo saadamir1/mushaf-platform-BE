43a24911f3190d3b892c2adbaca6717859fa5b46
Saad Amir <Saadamir070@gmail.com>
2026-02-25 00:04:44 +0500

    feat(bookmark): add verse column in entity for verse details

--- src/bookmarks/entities/bookmark.entity.ts
+++ src/bookmarks/entities/bookmark.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, CreateDateColumn, Unique } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
+import { Verse } from '../../quran/entities/verse.entity';
 
 @Entity('bookmarks')
 @Unique(['userId', 'verseId'])
@@ -19,6 +20,10 @@ export class Bookmark {
   @ManyToOne(() => User)
   @JoinColumn({ name: 'userId' })
   user: User;
+
+  @ManyToOne(() => Verse, { eager: true })
+  @JoinColumn({ name: 'verseId' })
+  verse: Verse;
 
   @CreateDateColumn()
   createdAt: Date;
